fa58ad7223c0793e4a2eb01b6301441cc5fd3e94
solp3rez <maryluzllusco03@gmail.com>
2025-09-22 14:58:53 -0300

    grilla termianda agregando algo mas

--- Grilla/src/complements/Grilla.jsx
+++ Grilla/src/complements/Grilla.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Grilla.css"; // tu CSS de la grilla
+//firebase storage
 
 const cards = [
   {
     name: "Matematicas",
     img: "https://i.pinimg.com/736x/11/27/fc/1127fc5b3431a9291c71c10445969741.jpg",
-    className: "Fernando Michalski",
+    className: "Laura Carrizo",
   },
   {
     name: "Ingles",
     img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Gabriel Veronda",
+    className: "Gabriel Beronda",
   },
   {
     name: "Quimica",
@@ -18,27 +19,27 @@ const cards = [
     className: "Marcela Gleiser",
   },{
     name: "Base de Datos",
-    img: "https://i.pinimg.com/736x/11/27/fc/1127fc5b3431a9291c71c10445969741.jpg",
-    className: "Fernando Michalski",
+    img: "https://i.pinimg.com/736x/f3/78/fe/f378fe13f884fa9417c0ebff36ab05fd.jpg",
+    className: "Tomas Mayorga",
   },
   {
     name: "AED",
-    img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Gabriel Veronda",
+    img: "https://i.pinimg.com/736x/96/27/93/9627931ce425e9483e7c1c1647f53a5e.jpg",
+    className: "Mauricio Uribe",
   },
   {
     name: "TAP",
-    img: "https://i.pinimg.com/1200x/52/22/e3/5222e3de01d704ede991a1842c6e6ab4.jpg",
+    img: "https://i.pinimg.com/736x/cf/db/70/cfdb70005d1b9930354fc0166b936b3f.jpg",
     className: "Marcela Gleiser",
   },{
     name: "Programacion Web",
-    img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Gabriel Veronda",
+    img: "https://i.pinimg.com/736x/00/5d/c1/005dc1e96cbd4b8e4755c500c34118bb.jpg",
+    className: "Federico Villace",
   },
   {
     name: "Diseño de Software",
-    img: "https://i.pinimg.com/1200x/52/22/e3/5222e3de01d704ede991a1842c6e6ab4.jpg",
-    className: "Marcela Gleiser",
+    img: "https://i.pinimg.com/736x/3d/6a/6c/3d6a6c4ea071378b66b3c6530af3b78c.jpg",
+    className: "Andres Navarro",
   },
   
 ];
---
+++ Grilla/src/complements/firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
